c512ae71307ed831f94501dbf072a9c1ab0c8a67
Aziz Saifee <azizsaifee123@gmail.com>
2026-02-02 14:06:54 +0530

    Fresh Push

---
+++ Think Decor/Think_DecorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Think_DecorApp.swift
+//  Think Decor
+//
+//  Created by Aziz Saifee on 02/02/26.
+//
+
+import SwiftUI
+
+@main
+struct Think_DecorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
